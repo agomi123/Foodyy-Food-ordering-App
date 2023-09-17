a3b36ef70332bd5fc9d3d0fa4b60077c5df28a13
Himanshu Gupta <85097442+agomi123@users.noreply.github.com>
2023-09-18 02:58:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import cors from 'cors';
 dotenv.config();
 connectDB();
 const __filename= fileURLToPath(import.meta.url);
-const __dirname= path.dirname(__filname);
+const __dirname= path.dirname(__filename);
 const app = express();
 app.use(cors()); 
 app.use(express.json());
